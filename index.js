9926cb8eeeee11be8edf810e58d440152eb6cc8a
Pablo Gonzalez <pabgon0@gmail.com>
2020-12-02 00:35:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,3 +24,7 @@ let unleashDog = function unleashDog(dogName, dogBreed) {
 }
 
 let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+  routine.map(dogName, )
+}
